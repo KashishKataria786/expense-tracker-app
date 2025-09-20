826a7b3307420dfa144daaf4c35bdf5dd0135534
kashishkataria786 <offverse.business@gmail.com>
2025-09-20 21:09:31 +0600

    Refactor server setup for production environment and add Vercel configuration

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,10 @@ app.get("/", (req, res) => {
   res.send("Express + Prisma + Neon PostgreSQL running!".bgBlue);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
+if(process.env.NODE_ENV!=='production'){
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
