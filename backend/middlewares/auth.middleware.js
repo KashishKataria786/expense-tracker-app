9bb1c403ced5790c127db398aa8bb0f04348fdab
kashishkataria786 <offverse.business@gmail.com>
2025-09-20 18:56:16 +0600

    Added ReadOnly Policy middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,9 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export const authenticate = (req, res, next) => {
-  const authHeader = req.headers["authorization"]; // lowercase
-  console.log(authHeader);
-  const token = authHeader  // extract token from "Bearer <token>"
+  const authHeader = req.headers["authorization"]; 
+  const token = authHeader 
 
   if (!token) {
     return res.status(401).json({ error: "Please login to access content" });
@@ -11,9 +10,25 @@ export const authenticate = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // { id, role, iat, exp }
+    req.user = decoded; 
+    console.log("De",decoded)
     next();
   } catch (err) {
     return res.status(401).json({ error: "Invalid or expired token" });
   }
 };
+
+export const readOnlyPolicy= (req,res,next)=>{
+  const role = req.user.role;
+  if(!role)return res.status(500).json({error:"Server Error"});
+  try {
+    if(role==='READ ONLY'){
+      return res.status(401).json({error:"Not Authorized"});
+    }else{
+      req.user=req.user
+      next();
+    };
+  } catch (error) {
+    return res.status(401).json({error:"Inavlid or Expired Token"})
+  }
+}
--- backend/routes/transaction.route.js
+++ backend/routes/transaction.route.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import { addTransaction, deleteTransaction, editTransaction, getAllTransactions, getSingleTransaction } from '../controllers/transaction.controller.js';
-import { authenticate } from '../middlewares/auth.middleware.js';
+import { authenticate, readOnlyPolicy } from '../middlewares/auth.middleware.js';
 const transactionRouter = express.Router();
 
 
 transactionRouter.get("/",authenticate,getAllTransactions);
 
-transactionRouter.post("/add",authenticate,addTransaction);
+transactionRouter.post("/add",authenticate,readOnlyPolicy,addTransaction);
 
-transactionRouter.delete("/delete/:id",authenticate,deleteTransaction);
+transactionRouter.delete("/delete/:id",authenticate,readOnlyPolicy,deleteTransaction);
 
-transactionRouter.patch("/edit/:id",authenticate,editTransaction);
+transactionRouter.patch("/edit/:id",authenticate,readOnlyPolicy,editTransaction);
 
 transactionRouter.get("/:id",authenticate,getSingleTransaction);
 
